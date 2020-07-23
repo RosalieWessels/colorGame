29e27c089c8e14870a111dc1b1bc9f9b66c4d33e
Rosalie Wessels <rosalie.wessels@yahoo.com>
2020-07-23 11:03:36 -0700

    added the functionality for the leaderboard and sorted it

--- colorGame/leaderboardView.swift
+++ colorGame/leaderboardView.swift
@@ -7,10 +7,32 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+import Firebase
 
 struct leaderboardView: View {
+    
+    var db = Firestore.firestore()
+    @State var leaderboard = ["Leaderboard"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(leaderboard, id:\.self) { Text($0) }
+            .onAppear(perform: getData)
+    }
+    
+    func getData() {
+        db.collection("leaderboard").order(by: "score", descending: true).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("There was an error getting the documents", err)
+            }
+            else {
+                for document in querySnapshot!.documents {
+                    let name = document.get("name") as! String
+                    let score = document.get("score") as! Int
+                    self.leaderboard.append("\(name) : \(String(score))")
+                }
+            }
+        }
     }
 }
 
